0e712e5d35097b9eb171dcd4927c353cf87155f2
149segolte <37300847+149segolte@users.noreply.github.com>
2025-05-30 14:13:34 -0400

    refactor(lsp): add gopls, move terraform-ls

--- init.lua
+++ init.lua
@@ -714,9 +714,11 @@ require('lazy').setup({
       --        For example, to see the options for `lua_ls`, you could go to: https://luals.github.io/wiki/settings/
       local servers = {
         clangd = {},
-        -- gopls = {},
+        gopls = {},
         -- pyright = {},
         rust_analyzer = {},
+        svelte = {},
+        ['terraform-ls'] = {},
         -- ... etc. See `:help lspconfig-all` for a list of all the pre-configured LSPs
         --
         -- Some languages (like typescript) have entire language plugins that can be useful:
@@ -741,8 +743,6 @@ require('lazy').setup({
             },
           },
         },
-
-        svelte = {},
 
         pylsp = {
           settings = {
@@ -787,7 +787,6 @@ require('lazy').setup({
         'prettierd',
         'prettier',
         'hclfmt',
-        'terraform-ls',
       })
       require('mason-tool-installer').setup { ensure_installed = ensure_installed }
 
