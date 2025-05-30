5cf3086bf305fc158958a5793fa4b65c0478f29f
149segolte <37300847+149segolte@users.noreply.github.com>
2025-05-30 14:48:24 -0400

    chg: disable lua_ls warnings

--- init.lua
+++ init.lua
@@ -743,7 +743,7 @@ require('lazy').setup({
               },
               hint = { enable = true },
               -- You can toggle below to ignore Lua_LS's noisy `missing-fields` warnings
-              -- diagnostics = { disable = { 'missing-fields' } },
+              diagnostics = { disable = { 'missing-fields' } },
             },
           },
         },
